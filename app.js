8e05998d59beada73b21340acccdd9af912b5dc2
ChafeiYahya <yahya.chafei99@gmail.com>
2023-08-24 14:31:30 +0100

    one

---
+++ app.js
@@ -0,0 +1,8 @@
+const navbar = document.getElementById("navbar")
+const menuBtn = document.getElementById("menu-btn")
+menuBtn.addEventListener("click", () =>{
+    navbar.classList.toggle("open")
+}
+
+
+)
